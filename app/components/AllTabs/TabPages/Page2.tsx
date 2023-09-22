a09882af79f6e524b5117c4546a0bb79d3eea6f3
jeean0668 <gijeeankyung@naver.com>
2023-09-22 10:46:00 +0900

    modified

--- app/components/AllTabs/TabPages/Page2.tsx
+++ app/components/AllTabs/TabPages/Page2.tsx
@@ -7,9 +7,10 @@ const Page1 = () => {
             <p className="present-title">선물을 골라주세요!</p>
             <div className="present-picker">
                 <ul className="present-picker-ul">
-                    <li>h1</li>
-                    <li>h2</li>
-                    <li>h3</li>
+                    <div className="figure">
+                    </div>
+                    <div className="figure"></div>
+                    <div className="figure"></div>
                 </ul>
             </div>
         </div>
