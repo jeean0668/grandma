d2cd05aa234d6724dd618b5a148028f0b5975470
jeean0668 <gijeeankyung@unist.ac.kr>
2023-09-28 01:02:16 +0900

    modified

--- app/components/AllTabs/TabPages/Page1.tsx
+++ app/components/AllTabs/TabPages/Page1.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Image from "next/image"
 import "./PageStyle.css"
 import {prefix} from "../../../../config/config";
+import letterImage from "./letter.jpeg";
 const Page1 = () => {
     return (
         <div className="letter-wrapper">
@@ -9,7 +10,8 @@ const Page1 = () => {
             {/* First tab content will go here */}
             <p className="letter-title">할머니의 생신을 축하드립니다!</p>
             <div className="letter">
-            <Image src= '/grandma/letter.jpeg' unoptimized alt="birthday letter" width={400} height={400}/>
+            {/* <Image src= '/grandma/letter.jpeg' unoptimized alt="birthday letter" width={400} height={400}/> */}
+            <Image src= {letterImage} unoptimized alt="birthday letter" width={400} height={400}/>
             </div>
       </div>
     );
