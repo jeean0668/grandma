4a0c3b47e56475e1c41b90c844d3aac719852002
jeean0668 <gijeeankyung@unist.ac.kr>
2023-09-28 00:48:33 +0900

    modified

--- app/components/AllTabs/TabPages/Page1.tsx
+++ app/components/AllTabs/TabPages/Page1.tsx
@@ -9,7 +9,7 @@ const Page1 = () => {
             {/* First tab content will go here */}
             <p className="letter-title">할머니의 생신을 축하드립니다!</p>
             <div className="letter">
-            <Image src= "/letter.jpeg" alt="birthday letter" width={400} height={400}></Image>
+            <Image src= "/letter.jpeg" unoptimized alt="birthday letter" width={400} height={400}/>
             </div>
       </div>
     );
