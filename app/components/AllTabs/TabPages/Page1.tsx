a05b29f66056990a9875f5cc4b6a9669b7ba99ec
jeean0668 <gijeeankyung@unist.ac.kr>
2023-09-28 00:11:46 +0900

    modifeed

--- app/components/AllTabs/TabPages/Page1.tsx
+++ app/components/AllTabs/TabPages/Page1.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Image from "next/image"
 import "./PageStyle.css"
 import {prefix} from "../../../../config/config";
+import letter from "../../../../public/letter.jpeg";
 const Page1 = () => {
     return (
         <div className="letter-wrapper">
@@ -9,7 +10,7 @@ const Page1 = () => {
             {/* First tab content will go here */}
             <p className="letter-title">할머니의 생신을 축하드립니다!</p>
             <div className="letter">
-            <Image src= {`${prefix}/letter.jpeg`} alt="birthday letter" width={400} height={400}></Image>
+            <Image src= {letter} alt="birthday letter" width={400} height={400}></Image>
             </div>
       </div>
     );
