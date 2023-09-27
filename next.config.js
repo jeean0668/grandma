6426470158f66b442edad52c1c259c8631d9b36e
jeean0668 <gijeeankyung@unist.ac.kr>
2023-09-27 23:46:12 +0900

    ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "joonyoung-s-portfolio-page";
+
 const nextConfig = {
     output: 'export',
+    assetPrefix: !debug ? `/${repository}/` : "",
     trailingSlash: true, 
 }
 
